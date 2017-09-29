8a03a643eb05d6360c009071f0644651e502aa27
Thomas Proffen <thomasproffen@gmail.com>
2017-09-29 10:56:47 -0400

    Initial commit
    Modified FouierCamera pages for ORNL Neutron Trailer

---
+++ README.md
@@ -0,0 +1,17 @@
+# Diffraction Demo
+
+These are the scripts needed to run the diffraction demonstration as part of the 
+Neutron Trailer as part of the [ORNL Travelling Science Fair](https://orise.orau.gov/ornl-science-fair/). 
+
+Depending on security setting and the web browser, the camera will not work when the file is loaded 
+locally. If no Internet access is available, run a simple webserver by open a command prompt and 
+running the command:
+
+```
+cd <directory with web pages>
+python -m http.server 8080 
+```
+
+Now you can access the pages at [localhost:8080](http://localhost:8080/) from your web browser.
+
+
---
+++ scripts/FourierCamera.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var snapButton = document.getElementById('snap');
+var canvas = document.getElementById('canvas');
+var sample = document.getElementById('sampleArea');
+
+var context = canvas.getContext('2d');
+var sampleContext = sample.getContext('2d');
+
+// This is the capture size of the camera
+
+var width=512;
+var height=width;
+
+var videoElement = document.getElementById('video');
+navigator.mediaDevices.enumerateDevices().then(connectStream).then(setup).catch(handleError);	
+
+// Mapping key strokes
+
+document.onkeydown = function (e) {
+  e = e || window.event;
+  switch (e.which || e.keyCode) {
+        case 13 : snapImage();   // Enter key
+            break;
+  }
+}
+
+//================================================================================
+// Functions below
+//================================================================================
+
+function setup () {
+	
+	canvas.height=height; 
+	canvas.width=width;
+	sample.height=height/2;
+	sample.width=width/2;
+	determineSizes();
+
+	snapButton.addEventListener("click", snapImage, false);
+	window.addEventListener("resize", determineSizes, false);
+}
+
+function connectStream() {
+	
+	if (window.stream) {
+		window.stream.getTracks().forEach(function(track) {track.stop();});
+	}
+	
+	var constraints = {video: {width: {ideal: width}, height: {ideal: height}}};
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(function(mediaStream) {videoElement.srcObject = mediaStream;}).catch(handleError);
+}
+
+function snapImage () {
+	
+	context.drawImage(videoElement, 0, 0, width, height);
+	videoElement.pause();
+	fourierTransform();
+	videoElement.play();
+}
+
+function fourierTransform () {
+	
+	var img = context.getImageData(0, 0, width, height);
+	var i;
+	var ampReal = [];
+	var ampImag = [];
+	
+	FFT.init(width);
+	FrequencyFilter.init(width);
+	SpectrumViewer.init(context);
+	
+	for(var y=0; y<height; y++) {
+		i = y*width;
+		for(var x=0; x<width; x++) {
+			ampReal[i + x] = Math.sqrt(img.data[(i << 2) + (x << 2)]);
+			ampImag[i + x] = 0.0;
+		}
+	}
+	
+	FFT.fft2d(ampReal, ampImag); 					// calculate the 2D FFT
+    FrequencyFilter.swap(ampReal, ampImag); 		// origin in the middle
+	SpectrumViewer.render(ampReal, ampImag, true);	// render the result
+	sampleContext.drawImage(videoElement, 0, 0, width/2, height/2);
+}
+
+function determineSizes () {
+	
+	var padW=width+20;
+	var padH=200;
+	
+	var newWidth=window.innerWidth-padW;
+	var newHeight=height*(newWidth/width)+1;
+	
+	if (newHeight>(window.innerHeight-padH)) {
+		newHeight=window.innerHeight-padH;
+		newWidth=width*(newHeight/height)+1;
+	}
+	canvas.style.width=newWidth + 'px';
+	canvas.style.height=newHeight + 'px';
+	sample.style.width=newWidth/2 + 'px';
+	sample.style.height=newHeight/2 + 'px';
+}
+
+function handleError(error) {
+	console.log('navigator.getUserMedia error: ', error);
+}
